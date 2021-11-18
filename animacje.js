2a719043f5b684706f06ad3e9ac2f97f768528a1
Michał Janiec <79807141+Kolumna@users.noreply.github.com>
2021-11-18 14:02:23 +0100

    Dzien2
    Dodałem:
    -animowane tło
    -ziemię

---
+++ animacje.js
@@ -0,0 +1,18 @@
+let Animacje = 
+{
+    aktualizacja: function(dane)
+    {
+        Animacje.zadania.Niebo(dane);
+    },
+    zadania:
+    {
+        Niebo: function(dane)
+        {
+            dane.obiekty.niebo.x-=1;
+            if(dane.obiekty.niebo.x < -1440)
+            {
+                dane.obiekty.niebo.x = 0;
+            }
+        }
+    }
+}
